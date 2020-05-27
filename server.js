caccc99d909d23495d9849c66506eb404319285d
Huda Prasetyo <huda.prs2002@gmail.com>
2020-05-27 20:44:31 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ extended: false }));
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
